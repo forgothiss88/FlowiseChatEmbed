70d370faa4e83262301795025a4a829ff54379b4
Andrea Vitali <andrea.vitov@gmail.com>
2024-05-23 17:41:05 +0200

    move bottombar out of chatbot container

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -420,25 +420,25 @@ export const Bot = (props: BotProps & { class?: string } & UserProps) => {
           topbarColor={props.topbarColor}
           isFullPage={props.isFullPage}
         />
-        <Bottombar
-          backgroundColor={props.textInput?.backgroundColor}
-          textColor={props.textInput?.textColor}
-          placeholder={props.textInput?.placeholder}
-          sendButtonColor={props.textInput?.sendButtonColor}
-          fontSize={props.fontSize}
-          disabled={loading()}
-          getInputValue={userInput}
-          setInputValue={setUserInput}
-          onSubmit={handleSubmit}
-          isFullPage={props.isFullPage}
-          clearChat={clearChat}
-          isDeleteEnabled={messages().length > 1}
-          showStarterPrompts={starterPrompts().length > 0 && messages().length <= 1}
-          starterPrompts={starterPrompts}
-          promptClick={handleSubmit}
-          scrollToBottom={scrollToBottom}
-        />
       </div>
+      <Bottombar
+        backgroundColor={props.textInput?.backgroundColor}
+        textColor={props.textInput?.textColor}
+        placeholder={props.textInput?.placeholder}
+        sendButtonColor={props.textInput?.sendButtonColor}
+        fontSize={props.fontSize}
+        disabled={loading()}
+        getInputValue={userInput}
+        setInputValue={setUserInput}
+        onSubmit={handleSubmit}
+        isFullPage={props.isFullPage}
+        clearChat={clearChat}
+        isDeleteEnabled={messages().length > 1}
+        showStarterPrompts={starterPrompts().length > 0 && messages().length <= 1}
+        starterPrompts={starterPrompts}
+        promptClick={handleSubmit}
+        scrollToBottom={scrollToBottom}
+      />
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
     </>
   );
