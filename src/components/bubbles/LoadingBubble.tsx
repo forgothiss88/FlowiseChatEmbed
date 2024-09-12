7aceeced58f179012460322ea8a5e880a09bb10d
Andrea Vitali <andrea.vitov@gmail.com>
2024-09-12 17:02:14 +0200

    use bg-transparent for loading

--- src/components/bubbles/LoadingBubble.tsx
+++ src/components/bubbles/LoadingBubble.tsx
@@ -2,7 +2,7 @@ import { TypingBubble } from '@/components';
 
 export const LoadingBubble = () => (
   <div class="flex justify-start ml-2 mb-2 mt-5 items-start animate-fade-in host-container">
-    <span class="px-4 py-4 whitespace-pre-wrap max-w-full rounded-2xl chatbot-host-bubble" data-testid="host-bubble">
+    <span class="px-4 py-4 whitespace-pre-wrap max-w-full rounded-2xl bg-transparent" data-testid="host-bubble">
       <TypingBubble />
     </span>
   </div>
