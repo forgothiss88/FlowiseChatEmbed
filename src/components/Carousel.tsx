2f8d8ee95cd506e8795114783fe8f05770075599
Andrea Vitali <andrea.vitov@gmail.com>
2024-10-16 18:06:14 +0200

    new carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,16 +4,19 @@ import { DownArrow, LeftArrow, RightArrow, UpArrow } from './icons/Arrow';
 
 const ProductCard = (props: { isPrimary: boolean; product: ProductMetadata; onClick?: () => null }) => {
   return props.isPrimary ? (
-    <div class="w-3/5 flex flex-col pt-4 pr-2 pl-4" style={{ flex: '0 0 auto' }}>
-      <img class="w-full px-6 pt-6 pb-2 aspect-auto object-cover bg-white animate-fade-in" src={props.product.thumbnail_url} alt="" />
-      <p class="m-2 text-sm font-normal text-center">{props.product.name}</p>
+    <div>
+      <img class="w-full rounded-2xl aspect-auto object-cover bg-white animate-fade-in" src={props.product.thumbnail_url} alt="" />
+      <p class="text-xs font-normal text-start p-1">{props.product.name}</p>
+      <p class="text-base font-normal text-start p-1">{Math.round(props.products[currentSlide()].metadata.price)}€</p>
     </div>
   ) : (
-    <div onClick={props.onClick} class="w-2/5 flex flex-col pt-4 pl-2 pr-4" style={{ flex: '0 0 auto' }}>
-      <div class="my-auto">
-        <img class="w-full p-6 aspect-auto object-cover bg-white opacity-30 grayscale animate-fade-in" src={props.product.thumbnail_url} alt="" />
-        <p class="m-2 text-xs opacity-30 text-center">{props.product.name}</p>
-      </div>
+    <div class="content-center h-full" onClick={props.onClick}>
+      <img
+        class="w-full rounded-2xl aspect-auto object-cover bg-white opacity-30 grayscale animate-fade-in"
+        src={props.product.thumbnail_url}
+        alt=""
+      />
+      <p class="m-2 text-xs opacity-30 text-start">{props.product.name}</p>
     </div>
   );
 };
@@ -119,30 +122,24 @@ export const MultiPriceButton = (props: { price: number; url: string; otherPrice
 
 export const SinglePriceButton = (props: { purchaseButtonText: string; price: number; url: string } & PurchaseButtonAspect) => {
   return (
-    <div class="ml-4 flex flex-col">
-      <div
-        class="flex flex-row rounded-full"
+    <div class="flex flex-row w-full pr-3">
+      <Show when={props.url.includes('amazon') || props.url.includes('amzn')}>
+        <a class="bg-white rounded-2xl w-6 h-6 my-auto">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/240px-Amazon_icon.svg.png" class="w-6 h-6 p-1"></img>
+        </a>
+      </Show>
+      <a
+        href={props.url}
+        target="_blank"
+        class="w-full text-sm font-normal px-4 py-2 whitespace-nowrap self-center block text-center"
         style={{
           background: props.purchaseButtonBackgroundColor,
+          color: props.purchaseButtonTextColor,
           'border-color': props.purchaseButtonBackgroundColor,
         }}
       >
-        <Show when={props.url.includes('amazon') || props.url.includes('amzn')}>
-          <a class="bg-white rounded-2xl w-6 h-6 ml-1 my-auto">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Amazon_icon.svg/240px-Amazon_icon.svg.png" class="w-6 h-6 p-1"></img>
-          </a>
-        </Show>
-        <a
-          href={props.url}
-          target="_blank"
-          class="text-base font-medium px-3 py-2 whitespace-nowrap self-center"
-          style={{
-            color: props.purchaseButtonTextColor,
-          }}
-        >
-          {props.purchaseButtonText} {Math.round(props.price)}€
-        </a>
-      </div>
+        {props.purchaseButtonText}
+      </a>
     </div>
   );
 };
@@ -156,8 +153,8 @@ export const SingleProductShowcase = (props: { product: SourceProduct } & Purcha
           src={props.product.metadata.thumbnail_url}
           alt=""
         />
-        <p class="text-xs font-normal text-left p-1">{props.product.metadata.name}</p>
-        <p class="text-base font-normal text-left p-1">{Math.round(props.product.metadata.price)}€</p>
+        <p class="text-xs font-normal text-start p-1">{props.product.metadata.name}</p>
+        <p class="text-base font-normal text-start p-1">{Math.round(props.product.metadata.price)}€</p>
         <div class="pb-3 pt-1">
           <a
             href={props.product.metadata.item_url}
@@ -194,17 +191,21 @@ export const ProductCarousel = (props: ProductCarouselProps) => {
   return (
     <>
       <div class="text-poppins overflow-hidden w-full" style={{ background: props.backgroundColor }}>
-        <div ref={carousel} class="flex flex-row overflow-hidden w-full">
+        <div ref={carousel} class="flex flex-row overflow-hidden w-full p-3">
           <Show when={numProducts > 0}>
-            <ProductCard isPrimary={true} product={props.products[currentSlide()].metadata} />
+            <div class="w-3/5 pr-3">
+              <ProductCard isPrimary={true} product={props.products[currentSlide()].metadata} />
+            </div>
           </Show>
           <Show when={numProducts > 1 && currentSlide() + 1 < numProducts}>
-            <ProductCard isPrimary={false} product={props.products[currentSlide() + 1].metadata} onClick={nextSlide} />
+            <div class="w-2/5">
+              <ProductCard isPrimary={false} product={props.products[currentSlide() + 1].metadata} onClick={nextSlide} />
+            </div>
           </Show>
         </div>
       </div>
       <div class="pb-3 px-3 pt-2 flex flex-row w-full">
-        <div class="w-3/5 flex flex-row justify-center">
+        <div class="w-3/5">
           <Show when={props.enableMultipricing}>
             <MultiPriceButton
               purchaseButtonText={props.purchaseButtonText}
